c7747ce43f42cdd4e7d91d956ea26fc2129b716c
Daria Approvato <dariaapprovato@gmail.com>
2022-09-03 13:35:46 -0400

    Updated Music Player , added ability to randomize array , continous play

---
+++ index.js
@@ -0,0 +1,34 @@
+function processData(data) {
+    const songs = []
+    Object.entries(data).map(entry => {
+        let value = entry[1];
+        songs.push(value)
+        console.log(value);
+    });
+    const playSongs = songs.map(function (song) {
+        return song.music_uri
+    })
+
+    const awwDeeOhh = document.createElement("audio")
+    awwDeeOhh.id = 'song'
+    awwDeeOhh.src = playSongs[Math.floor(Math.random() * playSongs.length)]
+    awwDeeOhh.controls = "controls"
+    //awwDeeOhh.loop = true
+    document.body.appendChild(awwDeeOhh)
+
+    const nextSong = document.getElementById('song')
+    nextSong.onended = function() {
+    awwDeeOhh.src = playSongs[Math.floor(Math.random() * playSongs.length)]
+    awwDeeOhh.autoplay = true;
+    }
+
+    let showSongs = songs.map(function (song) {
+        return song.name
+        //returns array of objects , key/value of name-USen: 'Agent K.K.' etc...
+    })
+    console.log(showSongs)
+}
+
+fetch ('http://acnhapi.com/v1/songs/')
+.then((response) => response.json())
+.then((data) => processData(data));

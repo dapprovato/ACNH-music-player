6de569b58315d0585e8f4c99eb45b4871d0ff304
Daria Approvato <dariaapprovato@gmail.com>
2022-09-15 23:44:29 -0400

    Refactored

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-// Initialize global variables
 let playSongs = []
 let currentIndex = 0
+
+//REFACTOR - -
+const songs = []
+const awwDeeOhh = document.createElement("audio")
+awwDeeOhh.id = 'song'
+const songTitle = document.createElement('div')
+songTitle.id = 'name'
+let showSongs = []
+
+function getNewRandomSong() {
+    currentIndex = Math.floor(Math.random() * playSongs.length)
+    song.src = playSongs[currentIndex]   
+    songTitle.textContent = showSongs[currentIndex]
+    song.autoplay = true;
+}
+// - - REFACTOR
 
 const playPauseBtn = document.getElementById("play_and_pause")
 const skipBtn = document.getElementById('skip')
@@ -8,14 +23,13 @@ const skipBtn = document.getElementById('skip')
 fetch ('http://acnhapi.com/v1/songs/')
 .then((response) => response.json())
 .then((data) => processData(data))
+.catch((error) => console.log(error))
+
 
 function processData(data) {
-    const songs = []
+    // const songs = []
 
-    // Object.entries turn objects into array , ex:
-    // {key: value, key2: value2} --> [['key', 'value'], ['key2', 'value2']]
     Object.entries(data).map(entry => {
-        // entry[0] is unnecessary info
         let songData = entry[1]
         songs.push(songData)
         console.log(songData)
@@ -25,69 +39,124 @@ function processData(data) {
         return song.music_uri
     })
 
-    const awwDeeOhh = document.createElement("audio")
-    awwDeeOhh.id = 'song'
+    // const awwDeeOhh = document.createElement("audio")
+    // awwDeeOhh.id = 'song'
     currentIndex = Math.floor(Math.random() * playSongs.length)
     awwDeeOhh.src = playSongs[currentIndex]
     document.body.appendChild(awwDeeOhh)
 
-    const showSongs = songs.map(function (song) {
+    // const showSongs = songs.map(function (song) {
+    //     return song.name['name-USen']
+    // })
+
+    //refactor
+    showSongs = songs.map(function (song) {
         return song.name['name-USen']
     })
-    const songTitle = document.createElement('div')
-    songTitle.id = 'name'
+
+    // const songTitle = document.createElement('div')
+    // songTitle.id = 'name'
     document.body.appendChild(songTitle)
     songTitle.textContent = showSongs[currentIndex]
 
+    //refactor
+    // function getNewRandomSong() {
+    //     currentIndex = Math.floor(Math.random() * playSongs.length)
+    //     song.src = playSongs[currentIndex]   
+    //     songTitle.textContent = showSongs[currentIndex]
+    //     awwDeeOhh.autoplay = true;
+// }
+
     const nextSong = document.getElementById('song')
     nextSong.onended = function() {
-        currentIndex = Math.floor(Math.random() * playSongs.length)
-        awwDeeOhh.src = playSongs[currentIndex]
-        songTitle.textContent = showSongs[currentIndex]
-        awwDeeOhh.autoplay = true
+        // currentIndex = Math.floor(Math.random() * playSongs.length)
+        // awwDeeOhh.src = playSongs[currentIndex]
+        // songTitle.textContent = showSongs[currentIndex]
+        // awwDeeOhh.autoplay = true
+        getNewRandomSong()
     }
 
-    function skipSong() {
-        currentIndex = Math.floor(Math.random() * playSongs.length)
-        song.src = playSongs[currentIndex]
-        songTitle.textContent = showSongs[currentIndex]
-        awwDeeOhh.autoplay = true;
-    }
+    // function skipSong() {
+        // currentIndex = Math.floor(Math.random() * playSongs.length)
+        // song.src = playSongs[currentIndex]
+        // songTitle.textContent = showSongs[currentIndex]
+        // awwDeeOhh.autoplay = true;
+        //  getNewRandomSong()
+    // }
 
-    skipBtn.addEventListener('click', skipSong)
+    skipBtn.addEventListener('click', getNewRandomSong)
 
-    function keyPlayPause(event) {
-        // event.code references the keycode for which key is pressed
-        if (event.code === 'Space') {
-            if (song.paused === true) {
-                song.play()
-                playPauseBtn.innerHTML = "⌷⌷"
-            } else {
-                song.pause()
-                playPauseBtn.innerHTML = "▷"
-            }
-        }
-            if (event.code === 'Enter') {
-                currentIndex = Math.floor(Math.random() * playSongs.length)
-                song.src = playSongs[currentIndex]
-                songTitle.textContent = showSongs[currentIndex]
-                awwDeeOhh.autoplay = true;
-        }
-    }
-    addEventListener('keypress', keyPlayPause)
+    // function keyPlayPause(event) {
+    //    // event.code references the keycode for which key is pressed
+    //     if (event.code === 'Space') {
+    //         if (song.paused === true) {
+    //             song.play()
+    //             playPauseBtn.innerHTML = "⌷⌷"
+    //         } else {
+    //             song.pause()
+    //             playPauseBtn.innerHTML = "▷"
+    //         }
+    //     }
+    //         if (event.code === 'Enter') {
+    //             // currentIndex = Math.floor(Math.random() * playSongs.length)
+    //             // song.src = playSongs[currentIndex]
+    //             // songTitle.textContent = showSongs[currentIndex]
+    //             // awwDeeOhh.autoplay = true;
+    //             getNewRandomSong()
+    //     }
+    // }
+    // addEventListener('keypress', keyPlayPause)
 }
 
-function playOrPauseSong() {
+//REFACTOR - -
+function playPauseRefact() {
     if (song.paused === true) {
-        song.play()
-        playPauseBtn.innerHTML = "⌷⌷"
-    } else {
-        song.pause()
-        playPauseBtn.innerHTML = "▷"
-    }
+            song.play()
+            playPauseBtn.innerHTML = "⌷⌷"
+        } else {
+            song.pause()
+            playPauseBtn.innerHTML = "▷"
+        }
 }
 
-playPauseBtn.addEventListener('click', playOrPauseSong)
+function keyPlayPause(event) {
+     if (event.code === 'Space') {
+        //  if (song.paused === true) {
+        //      song.play()
+        //      playPauseBtn.innerHTML = "⌷⌷"
+        //  } else {
+        //      song.pause()
+        //      playPauseBtn.innerHTML = "▷"
+        //  }
+        playPauseRefact()
+     }
+         if (event.code === 'Enter') {
+             // currentIndex = Math.floor(Math.random() * playSongs.length)
+             // song.src = playSongs[currentIndex]
+             // songTitle.textContent = showSongs[currentIndex]
+             // awwDeeOhh.autoplay = true;
+             getNewRandomSong()
+     }
+ }
+ addEventListener('keypress', keyPlayPause)
+// - - REFACTOR
+
+
+// function playOrPauseSong() {
+//  if (song.paused === true) {
+//         song.play()
+//         playPauseBtn.innerHTML = "⌷⌷"
+//     } else {
+//         song.pause()
+//         playPauseBtn.innerHTML = "▷"
+//     }
+// }
+
+// playPauseBtn.addEventListener('click', playOrPauseSong)
+
+// REFACTOR --
+playPauseBtn.addEventListener('click', playPauseRefact)
+// - - REFACTOR
 
 function cursorIcon(event) {
 const cursorImg = document.getElementById("cursor")
